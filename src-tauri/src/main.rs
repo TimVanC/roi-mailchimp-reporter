0e05cdedc95972e94ff0ea234529c490d546a7aa
TimVanC <timvancau@gmail.com>
2025-06-11 12:51:31 -0400

    fix: correct window management API usage

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
-  roi_mailchimp_reporter_lib::run();
+  tauri::Builder::default()
+    .setup(|app| {
+      #[cfg(debug_assertions)]
+      if let Some(window) = app.windows().get("main") {
+        window.open_devtools();
+      }
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
